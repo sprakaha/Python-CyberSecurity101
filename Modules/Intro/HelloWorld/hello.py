16aa4c902d696ce6e66c12234349cbf79259034f
Salena <sprakaha@gmail.com>
2021-08-16 12:30:55 -0400

    intro v1

---
+++ Modules/Intro/HelloWorld/hello.py
@@ -0,0 +1,8 @@
+# if we needed to import external libraries we would do so here
+# no imports are needed for this project
+
+# ask your question, get the user input and store in variable 
+name = input("What is your name?")
+
+# greet the user with their name
+print("hello, " + name)
